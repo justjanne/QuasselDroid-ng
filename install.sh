dbe8c16b628a28d8dd1010e774b01a8ad5496151
Janne Koschinski <janne@kuschku.de>
2016-02-04 22:53:06 +0100

    Updated install script (uses ANDROID_HOME for sdk location now)

--- install.sh
+++ install.sh
@@ -1,16 +1,12 @@
 #!/bin/sh
-cp local.properties aspm/local.properties
 cd AndroidSlidingUpPanel
 gradle build
 cd ..
-rm aspm/local.properties
 cp AndroidSlidingUpPanel/library/build/outputs/aar/library-release.aar app/libs/
 
-cp local.properties aspm/local.properties
 cd aspm
 gradle build jar shadowJar
 cd ..
-rm aspm/local.properties
 cp aspm/annotations/build/libs/annotations.jar app/libs/
 cp aspm/compiler/build/libs/compiler-all.jar app/libs/
 
