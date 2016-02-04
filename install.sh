976355d26ce35b8e6a152173501b484611f0c854
Janne Koschinski <janne@kuschku.de>
2016-02-05 00:03:21 +0100

    Updated install script to clean up first

--- install.sh
+++ install.sh
@@ -9,7 +9,7 @@ mkdir -p app/libs/
 # Build AndroidSlidingUpPanel
 echo sdk.dir=$ANDROID_HOME > AndroidSlidingUpPanel/local.properties
 cd AndroidSlidingUpPanel
-gradle build
+gradle clean build
 cd ..
 cp AndroidSlidingUpPanel/library/build/outputs/aar/library-release.aar app/libs/library-release.aar
 rm AndroidSlidingUpPanel/local.properties
@@ -17,7 +17,7 @@ rm AndroidSlidingUpPanel/local.properties
 # Build aspm
 echo sdk.dir=$ANDROID_HOME > aspm/local.properties
 cd aspm
-gradle build jar shadowJar
+gradle clean build jar shadowJar
 cd ..
 cp aspm/annotations/build/libs/annotations.jar app/libs/annotations.jar
 cp aspm/compiler/build/libs/compiler-all.jar app/libs/compiler-all.jar
@@ -25,5 +25,5 @@ rm aspm/local.properties
 
 # Build QuasselDroidNG
 echo sdk.dir=$ANDROID_HOME > local.properties
-gradle assembleRelease -x lintVitalRelease
+gradle clean assembleRelease -x lintVitalRelease
 rm local.properties
