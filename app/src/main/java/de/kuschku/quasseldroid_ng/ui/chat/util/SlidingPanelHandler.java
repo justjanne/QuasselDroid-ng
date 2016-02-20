0e5a895f339a2661c574e3bd2cff4c5f0d5a8f96
Janne Koschinski <janne@kuschku.de>
2016-02-20 22:54:12 +0100

    Tried yet another fix for the input defocus issue.

--- app/src/main/java/de/kuschku/quasseldroid_ng/ui/chat/util/SlidingPanelHandler.java
+++ app/src/main/java/de/kuschku/quasseldroid_ng/ui/chat/util/SlidingPanelHandler.java
@@ -216,10 +216,11 @@ public class SlidingPanelHandler {
         chatline.setOnKeyListener((v, keyCode, event) -> {
             if (event.getAction() == KeyEvent.ACTION_DOWN && (keyCode == KeyEvent.KEYCODE_ENTER || keyCode == KeyEvent.KEYCODE_NUMPAD_ENTER)) {
                 sendInput();
+                return true;
             }
 
             // Always return false to make sure we don’t lose focus
-            return true;
+            return false;
         });
     }
 
