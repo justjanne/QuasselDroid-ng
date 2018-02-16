eee9ad373f769e357bfd3a6bcd09e44e3ecbdd91
Janne Koschinski <janne@kuschku.de>
2018-02-16 17:42:26 +0100

    Make connection unit test work again

--- lib/src/test/java/de/kuschku/libquassel/ConnectionUnitTest.kt
+++ lib/src/test/java/de/kuschku/libquassel/ConnectionUnitTest.kt
@@ -1,11 +1,9 @@
 package de.kuschku.libquassel
 
-import de.kuschku.libquassel.protocol.ClientData
-import de.kuschku.libquassel.protocol.Protocol
-import de.kuschku.libquassel.protocol.Protocol_Feature
-import de.kuschku.libquassel.protocol.Quassel_Feature
+import de.kuschku.libquassel.protocol.*
 import de.kuschku.libquassel.quassel.ProtocolFeature
 import de.kuschku.libquassel.quassel.QuasselFeature
+import de.kuschku.libquassel.session.BacklogStorage
 import de.kuschku.libquassel.session.Session
 import de.kuschku.libquassel.session.SocketAddress
 import de.kuschku.libquassel.util.compatibility.reference.JavaHandlerService
@@ -46,7 +44,13 @@ class ConnectionUnitTest {
       }
 
       override fun getAcceptedIssuers(): Array<X509Certificate> = emptyArray()
-    }, SocketAddress(host, port), JavaHandlerService(), user to pass)
+    }, SocketAddress(host, port), JavaHandlerService(), object : BacklogStorage {
+      override fun storeMessages(vararg messages: Message, initialLoad: Boolean) = Unit
+      override fun storeMessages(messages: Iterable<Message>, initialLoad: Boolean) = Unit
+      override fun clearMessages(bufferId: BufferId, idRange: IntRange) = Unit
+      override fun clearMessages(bufferId: BufferId) = Unit
+      override fun clearMessages() = Unit
+    }, user to pass)
     session.join()
   }
 }
