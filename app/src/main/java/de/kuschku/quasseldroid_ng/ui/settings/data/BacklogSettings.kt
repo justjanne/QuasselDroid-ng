ed83aa656902516ce974d11751549b22786d01da
Janne Koschinski <janne@kuschku.de>
2018-03-08 16:00:46 +0100

    Further autocomplete changes

--- app/src/main/java/de/kuschku/quasseldroid_ng/ui/settings/data/BacklogSettings.kt
+++
@@ -1,9 +0,0 @@
-package de.kuschku.quasseldroid_ng.ui.settings.data
-
-data class BacklogSettings(
-  val dynamicAmount: Int = 20
-) {
-  companion object {
-    val DEFAULT = BacklogSettings()
-  }
-}
--- app/src/main/java/de/kuschku/quasseldroid_ng/ui/settings/data/ConnectionSettings.kt
+++
@@ -1,9 +0,0 @@
-package de.kuschku.quasseldroid_ng.ui.settings.data
-
-data class ConnectionSettings(
-  val showNotification: Boolean = true
-) {
-  companion object {
-    val DEFAULT = ConnectionSettings()
-  }
-}
