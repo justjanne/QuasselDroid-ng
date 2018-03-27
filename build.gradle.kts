f378d1ccaad0a751e8d88955324314d586f29735
Janne Koschinski <janne@kuschku.de>
2018-03-27 21:03:03 +0200

    Updated android gradle plugin

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ buildscript {
     jcenter()
   }
   dependencies {
-    classpath("com.android.tools.build:gradle:3.0.1")
+    classpath("com.android.tools.build:gradle:3.1.0")
     withVersion("1.2.31") {
       classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:$version")
       classpath("org.jetbrains.kotlin:kotlin-android-extensions:$version")
