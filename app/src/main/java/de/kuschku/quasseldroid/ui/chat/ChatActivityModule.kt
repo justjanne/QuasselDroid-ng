0dc4a8e940544d5bbbad548f26721c2697523657
Janne Koschinski <janne@kuschku.de>
2018-04-07 00:40:06 +0200

    Fixing a DI resolution bug introduced in last version

--- app/src/main/java/de/kuschku/quasseldroid/ui/chat/ChatActivityModule.kt
+++ app/src/main/java/de/kuschku/quasseldroid/ui/chat/ChatActivityModule.kt
@@ -1,6 +1,12 @@
 package de.kuschku.quasseldroid.ui.chat
 
+import dagger.Binds
 import dagger.Module
+import de.kuschku.quasseldroid.ui.chat.messages.MessageRenderer
+import de.kuschku.quasseldroid.ui.chat.messages.QuasselMessageRenderer
 
 @Module
-abstract class ChatActivityModule
+abstract class ChatActivityModule {
+  @Binds
+  abstract fun bindMessageRenderer(messageRenderer: QuasselMessageRenderer): MessageRenderer
+}
