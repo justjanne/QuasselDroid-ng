bce6dd8ea97f7411c6e41ebe259fc976a50fde4c
Janne Koschinski <janne@kuschku.de>
2018-04-28 03:08:37 +0200

    Fix a crash where DI was not correctly applied
    Signed-off-by: Janne Koschinski <janne@kuschku.de>

--- app/src/main/java/de/kuschku/quasseldroid/ui/setup/SlideFragment.kt
+++ app/src/main/java/de/kuschku/quasseldroid/ui/setup/SlideFragment.kt
@@ -28,14 +28,14 @@ import android.arch.lifecycle.Observer
 import android.os.Bundle
 import android.support.annotation.StringRes
 import android.support.design.widget.CollapsingToolbarLayout
-import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import dagger.android.support.DaggerFragment
 import de.kuschku.quasseldroid.R
 
-abstract class SlideFragment : Fragment() {
+abstract class SlideFragment : DaggerFragment() {
   @get:StringRes
   protected abstract val title: Int
   @get:StringRes
