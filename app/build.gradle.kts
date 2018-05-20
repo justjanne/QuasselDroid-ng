9f3d8568ae646967d8cfca18d30ada6c620ceafc
Janne Koschinski <janne@kuschku.de>
2018-05-20 14:19:56 +0200

    Added more translations

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -63,7 +63,7 @@ android {
 
     signingConfig = signingConfigs.findByName("default")
 
-    //resConfigs("en", "de", "fr-rCA", "lt", "pt")
+    resConfigs("en", "de", "fr-rCA", "lt", "pt", "sr")
 
     vectorDrawables.useSupportLibrary = true
 
