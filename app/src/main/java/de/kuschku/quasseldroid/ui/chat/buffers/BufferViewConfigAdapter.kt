df640aa57329ede2ded4824b5bcdd18134019861
Janne Koschinski <janne@kuschku.de>
2018-06-10 11:36:14 +0200

    Minor cleanup

--- app/src/main/java/de/kuschku/quasseldroid/ui/chat/buffers/BufferViewConfigAdapter.kt
+++ app/src/main/java/de/kuschku/quasseldroid/ui/chat/buffers/BufferViewConfigAdapter.kt
@@ -19,6 +19,7 @@
 
 package de.kuschku.quasseldroid.ui.chat.buffers
 
+import android.support.v7.view.ContextThemeWrapper
 import android.support.v7.widget.RecyclerView
 import android.support.v7.widget.ThemedSpinnerAdapter
 import android.view.LayoutInflater
@@ -29,7 +30,6 @@ import butterknife.BindView
 import butterknife.ButterKnife
 import de.kuschku.libquassel.quassel.syncables.BufferViewConfig
 import de.kuschku.quasseldroid.R
-import de.kuschku.quasseldroid.util.ui.ContextThemeWrapper
 import de.kuschku.quasseldroid.util.ui.RecyclerSpinnerAdapter
 
 class BufferViewConfigAdapter :
@@ -60,10 +60,8 @@ class BufferViewConfigAdapter :
   override fun onCreateViewHolder(parent: ViewGroup, dropDown: Boolean)
     : BufferViewConfigViewHolder {
     val inflater = LayoutInflater.from(
-      if (dropDown)
-        ContextThemeWrapper(parent.context, dropDownViewTheme)
-      else
-        parent.context
+      if (dropDown) ContextThemeWrapper(parent.context, dropDownViewTheme)
+      else parent.context
     )
     val view = inflater.inflate(R.layout.widget_spinner_item_toolbar, parent, false)
     return BufferViewConfigViewHolder(
