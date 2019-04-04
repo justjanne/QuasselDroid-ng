0c60098cca1d3c0c42833787727eef008c98743b
Janne Koschinski <janne@kuschku.de>
2019-04-04 16:42:59 +0200

    Further work on channel creation UI

--- app/src/main/java/de/kuschku/quasseldroid/ui/chat/add/create/ChannelCreateFragment.kt
+++ app/src/main/java/de/kuschku/quasseldroid/ui/chat/add/create/ChannelCreateFragment.kt
@@ -23,17 +23,68 @@ import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
+import androidx.appcompat.widget.AppCompatSpinner
+import androidx.appcompat.widget.SwitchCompat
+import androidx.lifecycle.Observer
+import butterknife.BindView
 import butterknife.ButterKnife
+import de.kuschku.libquassel.quassel.syncables.Network
+import de.kuschku.libquassel.quassel.syncables.interfaces.INetwork
 import de.kuschku.quasseldroid.R
-import de.kuschku.quasseldroid.util.service.ServiceBoundFragment
+import de.kuschku.quasseldroid.util.helper.combineLatest
+import de.kuschku.quasseldroid.util.helper.setDependent
+import de.kuschku.quasseldroid.util.helper.toLiveData
+import de.kuschku.quasseldroid.util.ui.settings.fragment.Savable
+import de.kuschku.quasseldroid.util.ui.settings.fragment.ServiceBoundSettingsFragment
 
-class ChannelCreateFragment : ServiceBoundFragment() {
+class ChannelCreateFragment : ServiceBoundSettingsFragment(), Savable {
+  @BindView(R.id.network)
+  lateinit var network: AppCompatSpinner
+
+  @BindView(R.id.name)
+  lateinit var name: EditText
+
+  @BindView(R.id.hidden)
+  lateinit var hidden: SwitchCompat
+
+  @BindView(R.id.invite_only)
+  lateinit var inviteOnly: SwitchCompat
+
+  @BindView(R.id.password_protected)
+  lateinit var passwordProtected: SwitchCompat
+
+  @BindView(R.id.password_group)
+  lateinit var passwordGroup: ViewGroup
+
+  @BindView(R.id.password)
+  lateinit var password: EditText
 
   override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
                             savedInstanceState: Bundle?): View? {
     val view = inflater.inflate(R.layout.add_create, container, false)
     ButterKnife.bind(this, view)
 
+    val networkAdapter = NetworkAdapter()
+    network.adapter = networkAdapter
+
+    viewModel.networks.switchMap {
+      combineLatest(it.values.map(Network::liveNetworkInfo)).map {
+        it.sortedWith(compareBy(String.CASE_INSENSITIVE_ORDER, INetwork.NetworkInfo::networkName))
+      }
+    }.toLiveData().observe(this, Observer {
+      if (it != null) {
+        networkAdapter.submitList(it)
+      }
+    })
+
+    passwordProtected.setDependent(passwordGroup)
+
     return view
+  }
+
+  override fun onSave(): Boolean {
+    // TODO: Implement
+    return false
   }
 }
---
+++ app/src/main/java/de/kuschku/quasseldroid/ui/chat/add/create/NetworkAdapter.kt
@@ -0,0 +1,88 @@
+/*
+ * Quasseldroid - Quassel client for Android
+ *
+ * Copyright (c) 2019 Janne Koschinski
+ * Copyright (c) 2019 The Quassel Project
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 3 as published
+ * by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.kuschku.quasseldroid.ui.chat.add.create
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.appcompat.widget.ThemedSpinnerAdapter
+import androidx.recyclerview.widget.RecyclerView
+import butterknife.BindView
+import butterknife.ButterKnife
+import de.kuschku.libquassel.protocol.NetworkId
+import de.kuschku.libquassel.quassel.syncables.interfaces.INetwork
+import de.kuschku.quasseldroid.R
+import de.kuschku.quasseldroid.util.ui.ContextThemeWrapper
+import de.kuschku.quasseldroid.util.ui.RecyclerSpinnerAdapter
+
+class NetworkAdapter : RecyclerSpinnerAdapter<NetworkAdapter.NetworkViewHolder>(),
+                       ThemedSpinnerAdapter {
+  var data = listOf<INetwork.NetworkInfo>()
+
+  fun submitList(list: List<INetwork.NetworkInfo>) {
+    data = list
+    notifyDataSetChanged()
+  }
+
+  override fun isEmpty() = data.isEmpty()
+
+  override fun onBindViewHolder(holder: NetworkViewHolder, position: Int) =
+    holder.bind(getItem(position))
+
+  override fun onCreateViewHolder(parent: ViewGroup, dropDown: Boolean)
+    : NetworkViewHolder {
+    val inflater = LayoutInflater.from(
+      if (dropDown)
+        ContextThemeWrapper(parent.context, dropDownViewTheme)
+      else
+        parent.context
+    )
+    val view = inflater.inflate(R.layout.widget_spinner_item_material, parent, false)
+    return NetworkViewHolder(view)
+  }
+
+  fun indexOf(id: NetworkId): Int? {
+    for ((key, item) in data.withIndex()) {
+      if (item.networkId == id) {
+        return key
+      }
+    }
+    return null
+  }
+
+  override fun getItem(position: Int): INetwork.NetworkInfo = data[position]
+  override fun getItemId(position: Int) = getItem(position).networkId.id.toLong()
+  override fun hasStableIds() = true
+  override fun getCount() = data.size
+  class NetworkViewHolder(itemView: View) :
+    RecyclerView.ViewHolder(itemView) {
+    @BindView(android.R.id.text1)
+    lateinit var text: TextView
+
+    init {
+      ButterKnife.bind(this, itemView)
+    }
+
+    fun bind(network: INetwork.NetworkInfo) {
+      text.text = network.networkName
+    }
+  }
+}
