1263f74a9b231b7ed9013bf936a448ee446d1bef
Janne Koschinski <janne@kuschku.de>
2019-08-12 13:13:42 +0200

    Fixes scrolling in chat editor

---
+++ app/src/main/java/de/kuschku/quasseldroid/util/ui/FixedNestingScrollView.kt
@@ -0,0 +1,42 @@
+/*
+ * Quasseldroid - Quassel client for Android
+ *
+ * Copyright (c) 2019 Janne Mareike Koschinski
+ * Copyright (c) 2019 The Quassel Project
+ *
+ * This program is free software: you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License version 3 as published
+ * by the Free Software Foundation.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package de.kuschku.quasseldroid.util.ui
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.MotionEvent
+import androidx.core.widget.NestedScrollView
+
+class FixedNestingScrollView : NestedScrollView {
+  constructor(context: Context) :
+    super(context)
+
+  constructor(context: Context, attrs: AttributeSet?) :
+    super(context, attrs)
+
+  constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) :
+    super(context, attrs, defStyleAttr)
+
+  override fun onInterceptTouchEvent(ev: MotionEvent?): Boolean {
+    if (!canScrollVertically(-1))
+      return false
+    return super.onInterceptTouchEvent(ev)
+  }
+}
