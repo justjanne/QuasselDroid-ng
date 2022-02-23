2392386214f51514806cdc7fe03e51f9774a477d
Janne Mareike Koschinski <janne@kuschku.de>
2022-02-23 19:19:04 +0100

    build: fix wrong java version

--- gradle/convention/build.gradle.kts
+++ gradle/convention/build.gradle.kts
@@ -13,3 +13,9 @@ dependencies {
   implementation("com.google.devtools.ksp:com.google.devtools.ksp.gradle.plugin:1.6.10-1.0.4")
   implementation("com.android.tools.build:gradle:7.1.1")
 }
+
+configure<JavaPluginExtension> {
+  toolchain {
+    languageVersion.set(JavaLanguageVersion.of(8))
+  }
+}
